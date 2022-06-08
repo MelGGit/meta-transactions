a7b5c1495f7b2fa41755dbff12a90539ff8b0448
Mel Grosser <melg@Mels-MacBook-Pro.local>
2022-06-08 11:33:07 +0200

    initial vue setup ts and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
