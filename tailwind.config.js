99a9f4c9dcd1801cda364cb2d38be16722d98689
Mel Grosser <grosser.mel@live.de>
2022-06-16 11:15:25 +0200

    remove old tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   mode: 'jit',
-  purge: [
-    './public/**/*.html',
-    './src/**/*.{js,jsx,ts,tsx,vue}',
-  ],
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
