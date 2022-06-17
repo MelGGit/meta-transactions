bdf58f86b4f58368f8c648be5cbaf5fcd691ac34
Mel Grosser <grosser.mel@live.de>
2022-06-17 08:57:52 +0200

    wording change

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,6 +3,6 @@
 <template>
     <header class="w-full flex flex-col justify-center items-center gap-2 text-white">
        <h2 class="text-4xl">Send your Message</h2>
-       <span>with meta-transactions</span>
+       <span>with a meta-transaction</span>
     </header>
 </template>
