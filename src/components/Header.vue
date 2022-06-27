fdaa763ea3aadc45f511e7f9c5fa60a0277cac3c
Mel Grosser <grosser.mel@live.de>
2022-06-27 09:24:11 +0200

    add github logo link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import GithubLogo from '../assets/GithubLogo.vue';
+
+</script>
 
 <template>
     <header class="w-full flex flex-col justify-center items-center gap-2 text-white">
        <h2 class="text-4xl">Send your Message</h2>
-       <span>with a meta-transaction</span>
+       <div class="flex gap-4">
+           <span>gasless using meta-transactions</span>
+           <a href="https://github.com/MelGGit/meta-transactions" target="_blank">
+               <GithubLogo class="fill-white" />
+           </a>
+       </div>
     </header>
 </template>
