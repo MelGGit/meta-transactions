dc12e4d9a60b84cc5a75502f788748d78d34dd8c
Mel Grosser <grosser.mel@live.de>
2022-06-27 09:08:29 +0200

    relayer information responsive

--- src/components/RelayerInformation.vue
+++ src/components/RelayerInformation.vue
@@ -8,9 +8,9 @@ defineProps<{
 </script>
 
 <template>
-    <div class="rounded-xl shadow-2xl p-4 bg-white flex flex-col gap-3 w-[30rem]">
+    <div class="sm:rounded-xl sm:shadow-2xl p-2 sm:p-4 bg-white flex flex-col gap-3 sm:w-[30rem] w-screen border-b border-grey-500 sm:border-none">
         <h2 class="font-bold text-lg">Relayer</h2>
-        <div class="flex gap-1 flex-col">
+        <div class="flex gap-1 flex-col text-sm sm:text-md">
             <span>Address: {{address}} </span>
             <span>Balance: {{balanceInEth.substring(0,10)}} ETH</span>
         </div>
