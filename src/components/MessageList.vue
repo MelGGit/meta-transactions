1cb7b4bf43b644a446ffe3acf55644b8719b2a57
Mel Grosser <grosser.mel@live.de>
2022-06-27 09:13:14 +0200

    responsive message list

--- src/components/MessageList.vue
+++ src/components/MessageList.vue
@@ -17,18 +17,18 @@ const createShortAddress = (address: string): string => {
 </script>
 
 <template>
-    <div class="rounded-xl shadow-2xl p-4 flex w-1/2 flex-col gap-4 bg-white h-96 overflow-y-scroll">
-        <div class="flex font-bold text-lg justify-evenly w-full items-center">
-            <span class="w-full text-center">Address</span>
+    <div class="sm:rounded-xl sm:shadow-2xl p-2 sm:p-4 flex sm:w-1/2 flex-col w-screen gap-4 bg-white sm:h-96 overflow-y-scroll">
+        <div class="flex font-bold text-lg sm:justify-evenly w-full items-center">
+            <span class="w-full sm:text-center">Address</span>
             <span class="w-full text-center">Message</span>
         </div>
-        <div v-if="messagesArray.length < 1" class="flex justify-evenly items-center">
-            <span class="w-full text-center">{{ createShortAddress('0x12349900ABCD') }}</span>
-            <span class="w-full text-center">Write the first message!</span>
+        <div v-if="messagesArray.length < 1" class="flex justify-evenly sm:items-center">
+            <span class="w-full sm:text-center">{{ createShortAddress('0x12349900ABCD') }}</span>
+            <span class="w-full sm:text-center">Write the first message!</span>
         </div>
         <ul class="w-full flex flex-col gap-2" v-if="messagesArray.length > 0">
-            <div class="flex w-full justify-evenly items-center" v-for="userMessage in messagesArray" :key="userMessage.transactionHash">
-                <span class="w-full text-center">
+            <div class="flex w-full sm:justify-evenly sm:items-center" v-for="userMessage in messagesArray" :key="userMessage.transactionHash">
+                <span class="w-full sm:text-center">
                     {{ createShortAddress(userMessage.args[0]) }}
                 </span>
                 <div class="w-full text-center overflow-ellipsis">
