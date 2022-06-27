63c1110c3522979f9312f47f96810bab659bbbca
Mel Grosser <grosser.mel@live.de>
2022-06-27 09:05:31 +0200

    responsive message input

--- src/components/MessageInput.vue
+++ src/components/MessageInput.vue
@@ -26,9 +26,9 @@ const sendTransaction = () => {
 </script>   
 
 <template>
- <div class="rounded-xl shadow-lg p-4 bg-white flex gap-2 flex-col">
+ <div class="sm:rounded-xl sm:shadow-lg p-1 sm:p-4 bg-white flex gap-2 flex-col w-screen sm:w-min border-b border-grey-500 sm:border-none">
      <form @submit.prevent="sendTransaction" class="flex gap-8">
-        <input type="text" v-model="messageComputed" class="border-gray-400 border rounded-xl px-4" />
+        <input type="text" v-model="messageComputed" class="border-gray-400 border rounded-xl sm:px-4 px-1" />
         <button class="bg-green-600 rounded-xl w-36 px-4 py-2 hover:bg-green-700 hover:disabled:bg-green-600  disabled:opacity-80" type="submit" :disabled="isSending || message.length < 1">{{isSending? 'Sending...' : 'Send Message'}}</button>
      </form>
      <button class="bg-[#1d2230] text-white rounded-xl w-36 px-4 py-2 disabled:opacity-30" :disabled="txHash.length < 1">
